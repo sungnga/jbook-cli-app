e47879e03604b7e2777107b6ca22ded04162ece7
sungnga <nga422@gmail.com>
2021-03-15 15:05:02 -0700

    Creating CellList and CellListItem components

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
 // import CodeCell from './components/CodeCell';
-import TextEditor from './components/TextEditor';
+// import TextEditor from './components/TextEditor';
+import CellList from './components/CellList';
 
 function App() {
 	return (
 		<Provider store={store}>
 			<div>
-				<TextEditor />
+				<CellList />
 			</div>
 		</Provider>
 	);
